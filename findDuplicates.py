c4ff1aabae4bc60094fa97e3e4115518530fbf0d
Gowthaman Ramasamy <gowthaman.ramasamy@seattlebiomed.org>
2014-12-10 20:55:59 -0800

    rename to findDuplicates.py; clean up; add comments

--- findDuplicates.py
+++ findDuplicates.py
@@ -1,15 +1,24 @@
 #!/Users/gramasamy/virtualenv/pSort/bin/python
-
+# author: Gowthaman Ramasamy
+# contact:
 # to execute this script from anywhere
 import sys, os
-sys.path.append('/Users/gramasamy/djcode/ngsdb03/')
+sys.path.append('/Users/gramasamy/djcode/picSort/')
 import time
 from os import path
-from collections import defaultdict
+from os import walk
 from os.path import basename
+from collections import defaultdict
+
+import shutil
+import argparse
+import imghdr
+import hashlib
+import exifread
+
 ####################################################################
 #
-# Functions
+# Argument handling
 #
 ####################################################################
 def argparse():
@@ -25,46 +34,62 @@ def argparse():
 
 ####################################################################
 #
-# main
+# functions
 #
 ####################################################################
 args = argparse()
 basepath = args.basepath
 print basepath
 
-from os import walk
-import imghdr
-import md5
 
 def get_filetype(filepath):
+    '''
+    finds file type
+    :param filepath: full path of an file
+    :return: type of the image file. If not an image file return "notImage"
+    '''
     fileType = imghdr.what(filepath)
     if not fileType:
         fileType = "notImage"
     return fileType
 
-import hashlib
 def hashfile(afile, hasher, blocksize=65536):
+    '''
+    creates sha hash (aka md5) for a file
+    :param afile: any file to get the sha (aka md5) value for
+    :param hasher: algorithm to be used to generate the hash
+    :param blocksize: read size
+    :return: sha key for the file
+    '''
     buf = afile.read(blocksize)
     while len(buf) > 0:
         hasher.update(buf)
         buf = afile.read(blocksize)
     return hasher.digest()
 
-import exifread
 def get_exif_value(fullFilePath, exifKey):
+    '''
+    gets the value for an exif tag (for an image file)
+    :param fullFilePath: input file path
+    :param exifKey: the exif tag for which values need to be parsed
+    :return: value for the exif tag; TagNotFound if that is not found.
+    '''
     # Open image file for reading (binary mode)
     file = open(fullFilePath, 'rb')
     tags = exifread.process_file(file, details=False, stop_tag=exifKey.split(" ")[1])
     if exifKey in tags:
         return (tags[exifKey])
     else:
-        return "NoValueExist"
+        return "TagNotFound"
 
 def isduplicate(path1, path2):
-    file1 = open(path1, 'rb')
-    file2 = open(path2, 'rb')
-    tags1 = exifread.process_file(file1, details=False)
-    tags2 = exifread.process_file(file2, details=False)
+    '''
+    checks if two files are same by several means
+    :param path1: path for file 1
+    :param path2: path for file 2
+    :return: YES/NO
+    '''
+
     areTheySame = 'YES'
 
     # is sha same
@@ -77,23 +102,37 @@ def isduplicate(path1, path2):
     size2 = os.path.getsize(path2)
     if size1 != size2:
         areTheySame = 'NO'
-    #print(tags1['EXIF DateTimeOriginal'], tags2['EXIF DateTimeOriginal'])
-    #print(tags2['EXIF DateTimeOriginal'])
-    #if tags1['EXIF DateTimeOriginal'] != tags2['EXIF DateTimeOriginal']:
-    #    areTheySame = 'NO'
+
+    # have same DateTimeOriginal
+    # file1 = open(path1, 'rb')
+    # file2 = open(path2, 'rb')
+    # tags1 = exifread.process_file(file1, details=False)
+    # tags2 = exifread.process_file(file2, details=False)
+    # print(tags1['EXIF DateTimeOriginal'], tags2['EXIF DateTimeOriginal'])
+    # print(tags2['EXIF DateTimeOriginal'])
+    # if tags1['EXIF DateTimeOriginal'] != tags2['EXIF DateTimeOriginal']:
+    #     areTheySame = 'NO'
 
     return areTheySame
 
-f = []
+
+####################################################################
+#
+# main
+#
+####################################################################
+
+'''create a lits of absolute paths for all the file under a base directory'''
+filePaths = []
 for (dirpath, dirnames, filenames) in walk(basepath):
     for filename in filenames:
         fullFilePath = os.path.join(dirpath, filename)
-        f.append(fullFilePath)
+        filePaths.append(fullFilePath)
 
 data = defaultdict(dict)
 
-for fullFilePath in f:
-    #print fullFilePath
+'''collect file attributes for each of the files (path)'''
+for fullFilePath in filePaths:
     fullFileType = get_filetype(fullFilePath)
     sha = hashfile(open(fullFilePath, 'rb'), hashlib.sha256())
     dateOriginal = "NA"
@@ -101,10 +140,6 @@ for fullFilePath in f:
     if fullFileType != 'notImage':
         dateOriginal = get_exif_value(fullFilePath, 'EXIF DateTimeOriginal')
         dateDigitized = get_exif_value(fullFilePath, 'EXIF DateTimeDigitized')
-        #print(dateOriginal)
-        #print(dateDigitized)
-        #print(fullFileType)
-        #print(type(dateOriginal))
         data[fullFilePath]['sha']=sha
         data[fullFilePath]['dateOriginal']=dateOriginal
         data[fullFilePath]['dateDigitized']=dateDigitized
@@ -113,18 +148,18 @@ for fullFilePath in f:
         data[fullFilePath]['fileName']=os.path.basename(fullFilePath)
 
 
-# Create duplicate bin if not there already
+'''Create duplicate bin if not there already'''
 if not os.path.exists(args.duplicatebin):
     os.makedirs(args.duplicatebin)
 
-#find duplicates
-import shutil
+'''find duplicates'''
 seen = defaultdict(dict)
 for path, exifdic in data.items():
     sha = data[path]['sha']
     if sha in seen:
         reallyDuplicate = isduplicate(path, seen[sha])
         if reallyDuplicate == 'YES':
+            ''' if duplicate copy all the files to duplicate bin'''
             print("duplicate:%s=>%s;%s=>%s" %(os.path.basename(path), os.path.basename(seen[sha]), path, seen[sha]))
             shutil.copy(path, args.duplicatebin)
             shutil.copy(seen[sha], args.duplicatebin)
